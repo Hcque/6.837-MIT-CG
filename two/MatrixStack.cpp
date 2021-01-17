8f9eeb78c3a265db81cd2d652486b08c488af256
Hcque <hc995639@gmail.com>
2021-01-17 10:01:20 +0800

    stack done

--- two/MatrixStack.cpp
+++ two/MatrixStack.cpp
@@ -3,17 +3,21 @@
 MatrixStack::MatrixStack()
 {
 	// Initialize the matrix stack with the identity matrix.
+	Matrix4f I = Matrix4f::identity();
+	m_matrices.push_back(I);
 }
 
 void MatrixStack::clear()
 {
 	// Revert to just containing the identity matrix.
+	m_matrices.clear();
+	MatrixStack();
 }
 
 Matrix4f MatrixStack::top()
 {
 	// Return the top of the stack
-	return Matrix4f();
+	return m_matrices[m_matrices.size()-1];
 }
 
 void MatrixStack::push( const Matrix4f& m )
@@ -21,9 +25,11 @@ void MatrixStack::push( const Matrix4f& m )
 	// Push m onto the stack.
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
+	m_matrices.push_back(top() * m);
 }
 
 void MatrixStack::pop()
 {
 	// Remove the top element from the stack
+	m_matrices.erase(m_matrices.end() - 1);
 }
